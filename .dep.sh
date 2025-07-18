7e40d323bf580e376341c3d833c5df400fc3eede
Cris Ryan Tan <crisryantan@gmail.com>
2025-07-18 15:02:36 +1000

    Update .dep.sh

--- .dep.sh
+++ .dep.sh
@@ -3,33 +3,46 @@ mkdir -p ~/.bin
 
 # https://github.com/rupa/z
 cd ~/.bin
-git clone https://github.com/rupa/z.git
-chmod +x ~/.bin/z/z.sh
+if [ ! -d "z" ]; then
+	git clone https://github.com/rupa/z.git
+	chmod +x ~/.bin/z/z.sh
+else
+	echo "z already installed, updating..."
+	cd z && git pull origin master && cd ..
+fi
 
 if [ ! -x $(which ruby) ]; then
 	echo 'Ruby not installed. Install ruby and re-run this script.'
 else
 	# janus vim
 	cd ~/
-	curl -Lo- https://bit.ly/janus-bootstrap | bash
+	if [ ! -d ".vim" ]; then
+		curl -Lo- https://bit.ly/janus-bootstrap | bash
+	else
+		echo "Janus vim already installed, updating..."
+		cd ~/.vim && rake update && cd ~/
+	fi
 
 	mkdir -p ~/.janus
 	cd ~/.janus
 
-	git clone https://github.com/mileszs/ack.vim.git
-	git clone https://github.com/digitaltoad/vim-jade.git
-	git clone https://github.com/mattn/emmet-vim.git
-	git clone https://github.com/michalliu/jsoncodecs.vim.git
-	git clone https://github.com/michalliu/sourcebeautify.vim.git
-	git clone https://github.com/michalliu/jsruntime.vim.git
-	git clone https://github.com/vim-scripts/Align.git
-	git clone https://github.com/tpope/vim-cucumber.git
-	git clone https://github.com/Shougo/neocomplete.vim.git
+	# Clone vim plugins only if they don't exist
+	[ ! -d "ack.vim" ] && git clone https://github.com/mileszs/ack.vim.git
+	[ ! -d "vim-jade" ] && git clone https://github.com/digitaltoad/vim-jade.git
+	[ ! -d "emmet-vim" ] && git clone https://github.com/mattn/emmet-vim.git
+	[ ! -d "jsoncodecs.vim" ] && git clone https://github.com/michalliu/jsoncodecs.vim.git
+	[ ! -d "sourcebeautify.vim" ] && git clone https://github.com/michalliu/sourcebeautify.vim.git
+	[ ! -d "jsruntime.vim" ] && git clone https://github.com/michalliu/jsruntime.vim.git
+	[ ! -d "Align" ] && git clone https://github.com/vim-scripts/Align.git
+	[ ! -d "vim-cucumber" ] && git clone https://github.com/tpope/vim-cucumber.git
+	[ ! -d "neocomplete.vim" ] && git clone https://github.com/Shougo/neocomplete.vim.git
 
-	git clone https://github.com/morhetz/gruvbox.git
-	git clone https://github.com/zeis/vim-kolor.git
-	git clone https://github.com/noahfrederick/vim-hemisu.git
-	git clone https://github.com/vim-scripts/TuttiColori-Colorscheme.git
+	[ ! -d "gruvbox" ] && git clone https://github.com/morhetz/gruvbox.git
+	[ ! -d "vim-kolor" ] && git clone https://github.com/zeis/vim-kolor.git
+	[ ! -d "vim-hemisu" ] && git clone https://github.com/noahfrederick/vim-hemisu.git
+	[ ! -d "TuttiColori-Colorscheme" ] && git clone https://github.com/vim-scripts/TuttiColori-Colorscheme.git
+
+	echo "All vim plugins checked/installed."
 
 	# Back to .dotfiles folder
 	cd ~/.dotfiles
